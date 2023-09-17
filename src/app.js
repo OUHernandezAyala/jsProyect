73afde8ee7d17db8bd50f8cb1bdd249e8b0d371c
OUHernandezAyala <142889499+OUHernandezAyala@users.noreply.github.com>
2023-09-17 04:09:51 +0000

    Upload Proyect

--- src/app.js
+++ src/app.js
@@ -8,4 +8,33 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let persona = new Array();
+  persona[0] = "Mi perro";
+  persona[1] = "Mi mamá";
+  persona[2] = "Mi hermana";
+
+  let accion = new Array();
+  accion[0] = "rompió";
+  accion[1] = "escondió";
+  accion[2] = "se llevo";
+
+  let objeto = new Array();
+  objeto[0] = "mi tarea";
+  objeto[1] = "mis llaves";
+  objeto[2] = "mi cartera";
+
+  let timepo = new Array();
+  timepo[0] = "ayer";
+  timepo[1] = "hoy";
+  timepo[2] = "durante la comida";
+  {
+    document.querySelector("#parte1").innerHTML =
+      persona[Math.floor(Math.random() * persona.length)];
+    document.querySelector("#parte2").innerHTML =
+      accion[Math.floor(Math.random() * accion.length)];
+    document.querySelector("#parte3").innerHTML =
+      objeto[Math.floor(Math.random() * objeto.length)];
+    document.querySelector("#parte4").innerHTML =
+      timepo[Math.floor(Math.random() * timepo.length)];
+  }
 };
